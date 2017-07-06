1ee35b83c27d21b5e115dfb843c736433f7e3db8
Brian Brooks <brian.brooks@acm.org>
2017-07-06 15:54:46 -0400

    base->y and 2.0->4.0

--- src/MySquare.java
+++ src/MySquare.java
@@ -1,13 +1,13 @@
 /**
  * Takes 1 double argument from stdin and outputs 
- * to stdout the square of the argument multiplied by 2.0.
+ * to stdout the square of the argument multiplied by 4.0.
  *
  */
 public class MySquare {
 
     public static void main(String[] args) throws NumberFormatException {
-    	double base = Double.parseDouble(args[0]);
-    	System.out.println( 2.0*Math.pow(base,2) );
+    	double y = Double.parseDouble(args[0]);
+    	System.out.println( 4.0*Math.pow(y,2) );
     }
 
 }
