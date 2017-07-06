d0318ef994ce8bae34db4eed30f5acaceacd6821
Brian Brooks <brian.brooks@acm.org>
2017-07-06 15:18:57 -0400

    add MySquare.java

---
+++ src/MySquare.java
@@ -0,0 +1,8 @@
+public class MySquare {
+
+    public static void main(String[] args) throws NumberFormatException {
+    	double base = Double.parseDouble(args[0]);
+    	System.out.println( Math.pow(base,2) );
+    }
+
+}
